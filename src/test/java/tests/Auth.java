2a8fd586f98310304dce69a03ed27cb038f36e2f
Orfik <orfiik@gmail.com>
2017-04-13 10:23:59 +0300

    Travis ci - config with Chrome 0.5

--- src/test/java/tests/Auth.java
+++ src/test/java/tests/Auth.java
@@ -18,7 +18,7 @@ import java.io.IOException;
 
 @Listeners(value = AllureTestListener.class)
 
-public class TestAuth extends BaseTest {
+public class Auth extends BaseTest {
 
     private SignInPage singIn;
     private StudioHomePage studioHome;
--- src/test/java/tests/TestCreationProject.java
+++ src/test/java/tests/TestCreationProject.java
@@ -16,7 +16,7 @@ import java.io.IOException;
 
 @Listeners(value = AllureTestListener.class)
 
-public class CreationProject extends BaseTest {
+public class TestCreationProject extends BaseTest {
 
 
     private DetailProjectPage detailProjectPage;
