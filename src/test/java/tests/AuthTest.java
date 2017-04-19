4c6c89cf8095f3e20288843668f8b08e5ec38122
Orfik <orfiik@gmail.com>
2017-04-19 13:06:36 +0300

    Test

--- src/test/java/tests/AuthTest.java
+++ src/test/java/tests/AuthTest.java
@@ -47,10 +47,9 @@ public class AuthTest extends BaseTest {
     @Features("Authorozation")
     @Stories("Successful authorization")
     @Description("successful authorization")
-    @Test(description = "successful authorization", dataProvider = "validUserData", dataProviderClass = DataProviderClass.class)
+    @Test(priority=1, description = "successful authorization", dataProvider = "validUserData", dataProviderClass = DataProviderClass.class)
     public void authSuccessfulAuth(String login, String password, String expectedUserName) throws IOException {
         auth(login, password);
-
         Assert.assertTrue(projectBoard.getUserName().contains(expectedUserName));
     }
 }
