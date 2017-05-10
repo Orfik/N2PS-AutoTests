49a3f1d3defa42cbe2672ad96cb3235a64eb10b4
Orfik <orfiik@gmail.com>
2017-05-10 16:54:00 +0300

    Running jetty server for allure reports

--- src/main/java/webDriver/PrepareDrivers.java
+++ src/main/java/webDriver/PrepareDrivers.java
@@ -7,14 +7,14 @@ public class PrepareDrivers {
     private static DesiredCapabilities capabilities;
 
     public static DesiredCapabilities prepareFirefox() {
-        System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "//src//test//resources//geckodriver_v.14.exe");
+        System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "//src//test//resources//geckodriver_v.14");
         capabilities = DesiredCapabilities.firefox();
         capabilities.setCapability("marionette", true);
         return capabilities;
     }
 
     public static DesiredCapabilities prepareChrome() {
-        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "//src//test//resources//chromedriver_for_win.exe");
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "//src//test//resources//chromedriver");
         capabilities = DesiredCapabilities.chrome();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("no-sandbox");
