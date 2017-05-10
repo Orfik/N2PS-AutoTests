078e9d9c98984d66d8e49f426c977baae2a7d047
Orfik <orfiik@gmail.com>
2017-05-10 16:45:10 +0300

    Last

--- src/main/java/webDriver/PrepareDrivers.java
+++ src/main/java/webDriver/PrepareDrivers.java
@@ -7,18 +7,17 @@ public class PrepareDrivers {
     private static DesiredCapabilities capabilities;
 
     public static DesiredCapabilities prepareFirefox() {
-        System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "//src//test//resources//geckodriver_v.14");
+        System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "//src//test//resources//geckodriver_v.14.exe");
         capabilities = DesiredCapabilities.firefox();
         capabilities.setCapability("marionette", true);
         return capabilities;
     }
 
     public static DesiredCapabilities prepareChrome() {
-        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "//src//test//resources//chromedriver");
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "//src//test//resources//chromedriver_for_win.exe");
         capabilities = DesiredCapabilities.chrome();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("no-sandbox");
-        //options.setBinary("C:\\Users\\Owner\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe");
         capabilities.setCapability(ChromeOptions.CAPABILITY, options);
         return capabilities;
     }
